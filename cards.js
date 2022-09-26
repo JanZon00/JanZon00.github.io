f2b247396dd279f95945de909c2d2ec5e50e8745
JanZon00 <112341667+JanZon00@users.noreply.github.com>
2022-09-26 12:34:24 +0200

    karty

--- cards.js
+++ cards.js
@@ -54,7 +54,7 @@ var x;
 const numbers = new Set();
 
 
-var imagesArray = ["deck/1.png", "deck/2.png", "deck/3.png", "deck/4.png", "deck/5.png", "deck/6.png", "deck/7.png", "deck/8.png", "deck/9.png", "deck/10.png", "deck/11.png", "deck/12.png", "deck/13.png", "deck/14.png", "deck/15.png", "deck/16png", "deck/17.png", "deck/18.png", "deck/19.png", "deck/20.png", "deck/21.png", "deck/22.png", "deck/23.png", "deck/24.png", "deck/25.png", "deck/26.png", "deck/27.png", "deck/28.png", "deck/29.png", "deck/30.png", "deck/31.png", "deck/32.png", "deck/33.png", "deck/34.png", "deck/35.png", "deck/36.png", "deck/37.png", "deck/38.png", "deck/39.png", "deck/40.png", "deck/41.png", "deck/42.png", "deck/43.png", "deck/44.png", "deck/45.png", "deck/46.png", "deck/47.png", "deck/48.png", "deck/49.png", "deck/50.png", "deck/51.png", "deck/52.png"];
+var imagesArray = ["deck/1.png", "deck/2.png", "deck/3.png", "deck/4.png", "deck/5.png", "deck/6.png", "deck/7.png", "deck/8.png", "deck/9.png", "deck/10.png", "deck/11.png", "deck/12.png", "deck/13.png", "deck/14.png", "deck/15.png", "deck/16.png", "deck/17.png", "deck/18.png", "deck/19.png", "deck/20.png", "deck/21.png", "deck/22.png", "deck/23.png", "deck/24.png", "deck/25.png", "deck/26.png", "deck/27.png", "deck/28.png", "deck/29.png", "deck/30.png", "deck/31.png", "deck/32.png", "deck/33.png", "deck/34.png", "deck/35.png", "deck/36.png", "deck/37.png", "deck/38.png", "deck/39.png", "deck/40.png", "deck/41.png", "deck/42.png", "deck/43.png", "deck/44.png", "deck/45.png", "deck/46.png", "deck/47.png", "deck/48.png", "deck/49.png", "deck/50.png", "deck/51.png", "deck/52.png"];
 
 function rangeFunction(){
 	var a = document.getElementById("cards").value;
@@ -65,11 +65,11 @@ function rangeFunction(){
 		
 		karta2 = Math.floor(Math.random() * 52);
 		karta3 = karta2;
-		document.canvas2.src = imagesArray[karta2];
 		
 		while(karta3 === karta2){
 			karta3 = Math.floor(Math.random() * 52);
 		}
+		document.canvas2.src = imagesArray[karta2];
 		document.canvas3.src = imagesArray[karta3];
 		document.canvas.src = src="blank.jpg";
 		document.canvas4.src = src="blank.jpg";
@@ -82,16 +82,17 @@ function rangeFunction(){
 		karta1 = Math.floor(Math.random() * 52);
 		karta2 = karta1;
 		karta3 = karta1;
-		document.canvas.src = imagesArray[karta1];
 		
-		while(karta2 === karta1){
+		while(karta2 == karta1){
 			karta2 = Math.floor(Math.random() * 52);
 		}
-		document.canvas2.src = imagesArray[karta2];
 		
-		while(karta3 === karta1 || karta3 === karta2){
+		
+		while(karta3 == karta1 || karta3 == karta2){
 			karta3 = Math.floor(Math.random() * 52);
 		}
+		document.canvas.src = imagesArray[karta1];
+		document.canvas2.src = imagesArray[karta2];
 		document.canvas3.src = imagesArray[karta3];
 	}
 	else{
@@ -104,23 +105,25 @@ function rangeFunction(){
 		karta2 = karta1;
 		karta3 = karta1;
 		karta4 = karta1;
-		document.canvas.src = imagesArray[karta1];
 		
-		while(karta2 === karta1){
+		while(karta2 == karta1){
 			karta2 = Math.floor(Math.random() * 52);
-		}
-		document.canvas2.src = imagesArray[karta2];
-		
-		while(karta3 === karta1 || karta3 === karta2){
+		}	
+		while(karta3 == karta1 || karta3 == karta2){
 			karta3 = Math.floor(Math.random() * 52);
 		}
-		document.canvas3.src = imagesArray[karta3];
-		
-		while(karta4 === karta1 || karta4 === karta2 || karta4 === karta3){
+		while(karta4 == karta1 || karta4 == karta2 || karta4 == karta3){
 			karta4 = Math.floor(Math.random() * 52);
 		}
+		document.canvas.src = imagesArray[karta1];
+		document.canvas2.src = imagesArray[karta2];
+		document.canvas3.src = imagesArray[karta3];
 		document.canvas4.src = imagesArray[karta4];
 	}
+	document.getElementById("demo").innerHTML = karta1;
+	document.getElementById("demo1").innerHTML = karta2;
+	document.getElementById("demo2").innerHTML = karta3;
+	document.getElementById("demo3").innerHTML = karta4;
 }
 
 window.addEventListener("keydown", checkKeyPress, false);
